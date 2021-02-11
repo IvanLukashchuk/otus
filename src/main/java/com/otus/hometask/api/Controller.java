b493d3a4dbadd8969831d852973643e8dd32bca8
Ivan Lukashchuk <ivan.lukashchuk@gmail.com>
2021-02-11 20:36:59 +0200

    Home task 2

--- src/main/java/com/otus/hometask/api/Controller.java
+++ src/main/java/com/otus/hometask/api/Controller.java
@@ -1,13 +1,22 @@
 package com.otus.hometask.api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
+    @Value("${spring.datasource.url}")
+    public String dbUrl;
+
     @GetMapping("health")
     public String health(){
         return "{\"status\": \"OK\"}";
+    }
+
+    @GetMapping("db")
+    public String dbUrl(){
+        return dbUrl;
     }
 }
